e38bef03346415eb1bce9749411fe7c25e2d4f92
Ihor Moroz <kusatel@gmail.com>
2018-12-11 16:17:28 +0200

    eeprom

--- DAC.ino
+++ DAC.ino
@@ -1,14 +1,10 @@
+#include <EEPROM.h>
 /**************************************************************************/
 /*!
-    @file     trianglewave.pde
-    @author   Adafruit Industries
-    @license  BSD (see license.txt)
-
+    @author   Ihor Moroz
     This example will generate a triangle wave with the MCP4725 DAC.
-
     This is an example sketch for the Adafruit MCP4725 breakout board
     ----> http://www.adafruit.com/products/935
-
     Adafruit invests time and resources providing this open source code,
     please support Adafruit and open-source hardware by purchasing
     products from Adafruit!
@@ -22,36 +18,27 @@ int count = 0;
 		bool down = 0;
 			int up_level(void);
 			int down_level(void);
-            int delta(int analog[], int digital[], int first_value);
-
 void setup(void) {
-  int first_dacValue = 0;
-  int dac_analog[280];
-  int dac_digital[280] = {1200};
+	/**
+	    // очистка EEPROM 
+		// clearing of EEPROM
+		for (int i = 0 ; i < EEPROM.length() ; i++) {
+		EEPROM.write(i, 0);
+		}
+	**/
   pinMode(8, INPUT);
   pinMode(9, INPUT);
   Serial.begin(9600);
   dac.begin(0x60);
   Serial.println("Generating a triangle wave");
-	  first_dacValue = analogRead(0);
-	  	Serial.print("first_dacValue - ");
-	  	Serial.println(first_dacValue);
-  			for(int i = 0; i < 280; i++){
-				dac_analog[i] = 300 + i;
-				if(i > 0)
-				dac_digital[i] = dac_digital[i - 1] + 5;
-			}
-			int del = delta(dac_analog, dac_digital, first_dacValue);
-			/**
-			Serial.println();
-			Serial.print("Delta - ");
-			Serial.print(del);
+			/** Трансформация массива EEPROM в четырехзначное число переменной count 
+				Transformation of EEPROM array to number
 			**/
-				count = first_dacValue + del;
-					/**
+				count = EEPROM[0]*1000+ EEPROM[1]*100 + EEPROM[2]*10 + EEPROM[3];
+					
 					Serial.print(" / count  - ");
 					Serial.println(count);
-					**/
+					
 }
 void loop(void) {
     uint32_t counter;
@@ -59,30 +46,35 @@ void loop(void) {
 		down = digitalRead(9);
 			(up) ? count = up_level(): count;
 			(down) ? count = down_level(): count;
-}
-
-int delta(int analog[], int digital[], int first_value){
-	int temp = 0;
-	int num = 0;
-	int delta = 0;
-	if(first_value < 300) {num = 0; }
-	if(first_value > 560) { num = 259; }
-	for(int i = 0; i < 280; i++){
-		temp = analog[i];
-			if(first_value == temp){
-				num = i;
-				break;
-			}
-	}
-		    delta = digital[num] - analog[num];
-								  return delta;
+			/** Трансформация переменной  count в массив EEPROM
+				Transformation of the count variable to EEPROM array
+			**/
+				EEPROM[0] = count/1000;   
+				EEPROM[1] = count/100%10; 
+				EEPROM[2] = count%100/10; 
+				EEPROM[3] = count%10; 
+				/**
+				count = EEPROM[0]*1000+ EEPROM[1]*100 + EEPROM[2]*10 + EEPROM[3];
+				if(up == 0 && down == 0){
+					Serial.print(EEPROM[0]);
+					Serial.print(EEPROM[1]);
+					Serial.print(EEPROM[2]);
+					Serial.print(EEPROM[3]);
+					Serial.print(" count - ");
+					Serial.println(count);
+				}
+				**/
 }
 int up_level(void){
 	for (int counter = 0; counter < 4095; counter++)
     {
-      delay(10);
-      (count < 1200) ? count = 1200 : count = count + 5;
-	   if(count > 2500) break;
+		delay(5);
+		(count < 1200) ? count = 1200 : count = count + 5;
+		/**
+		Serial.print(" / count  - ");
+		Serial.println(count);
+		**/
+		if(count > 2500){ count = 2500; break;}
 		dac.setVoltage(count, true);
 					   return count;
     }
@@ -90,10 +82,13 @@ int up_level(void){
 int down_level(void){
 	for (int counter = 4095; counter > 0; counter--)
     {
-      delay(10);
-	  (count > 2500) ? count = 2500 : count = count--;
-      count = count - 5;
-		if(count < 1100) break;
+        delay(5);
+	    (count > 2500) ? count = 2500 : count = count - 5;
+		/**
+	  	Serial.print(" / count  - ");
+		Serial.println(count);
+		**/
+		if(count < 1100) {count = 1100; break;}
 			dac.setVoltage(count, true);
 						   return count;
     }
